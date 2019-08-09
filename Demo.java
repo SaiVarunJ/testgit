46e75335c359ab095e0a1275f9bd0a42eb0f02f5
SaiVarunJ <40985914+SaiVarunJ@users.noreply.github.com>
2019-08-09 17:05:37 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 import java.io.*;
 public class Demo{
   public static void main(String []args){
-       System.out.println("Java is WORKING");
+       System.out.println("Java demo and git and jenkins is WORKING");
 }
 }
